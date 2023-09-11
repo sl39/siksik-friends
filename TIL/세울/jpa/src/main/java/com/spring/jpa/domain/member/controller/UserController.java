67854eb4db03e462f7f427234059abd3ff57cb82
swcl309 <swcl309@naver.com>
2023-09-12 02:51:55 +0900

    Docs: security3.0

--- TIL/세울/jpa/src/main/java/com/spring/jpa/domain/member/controller/UserController.java
+++ TIL/세울/jpa/src/main/java/com/spring/jpa/domain/member/controller/UserController.java
@@ -4,6 +4,7 @@ package com.spring.jpa.domain.member.controller;
 import com.spring.jpa.domain.member.UserCreateForm;
 import com.spring.jpa.domain.member.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,18 @@ public class UserController {
             bindingResult.rejectValue("password2","passwordInCorrect","2개의 패스워드가 일치하지 않습니다.");
             return "singup_form";
         }
-        userService.create(userCreateForm.getUsername(),userCreateForm.getEmail(),userCreateForm.getPassword1());
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "이미 등록된 사용자 입니다";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
         return "성공!";
     }
 }
