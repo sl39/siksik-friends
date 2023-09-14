be6111237105e1ddde8885caa13d55bcb4fe0ae4
구희영 <9gmldud@naver.com>
2023-09-14 15:34:16 +0900

    Feat: 메인 화면 사이징
    메인 화면 풀로 맞추고 home 생성

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "Elefante",
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import Face1 from "public/images/character/face1.png";
+// import Image from "next/image";
+import Background from "public/images/background.png";
 import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.main}>
+      {/* <img src={Background} width="100%" height="100%" alt="~" /> */}
       <h1>식식프렌즈</h1>
       <Link href="/login">로그인</Link>
-    </>
+    </div>
   );
 }
