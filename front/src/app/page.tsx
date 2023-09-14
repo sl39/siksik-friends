1b5c8259a6471ed8a57c02a5878733ef1bdd3f7e
구희영 <9gmldud@naver.com>
2023-09-14 12:24:42 +0900

    Update: lint 설정 추가 및 이미지 업로드 테스트
    이미지 업로드 테스트와 lint import 구문 엔터 룰 무시 추가

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import Face1 from "public/images/character/face1.png";
+
 export default function Home() {
-  return <h1>뿌롱뜨</h1>;
+  return (
+    <>
+      <h1>뿌롱뜨</h1>
+      <Image src={Face1} width="" height="" alt="" />
+    </>
+  );
 }
