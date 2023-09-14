eabc60602a98cf30b25816ed78ede1588bbfb289
구희영 <9gmldud@naver.com>
2023-09-14 12:37:52 +0900

    Style: 스타일 폴더 생성 후 관리 테스트
    전역 스타일 관리 css 생성 후 되는 지 확인

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import Face1 from "public/images/character/face1.png";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <h1>뿌롱뜨</h1>
       <Image src={Face1} width="" height="" alt="" />
+      <div className={styles.book} />
+      <div className={styles.page1} />
+      <div className={styles.page2} />
+      <div className={styles.page3} />
     </>
   );
 }
