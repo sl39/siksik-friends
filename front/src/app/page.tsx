0ce860fc4f5d00a7a23da5e8d7b50cf4519862c7
구희영 <9gmldud@naver.com>
2023-09-14 15:42:14 +0900

    Feat: Link Home
    Home 연결 링크 생성

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
     <div className={styles.main}>
       {/* <img src={Background} width="100%" height="100%" alt="~" /> */}
       <h1>식식프렌즈</h1>
-      <Link href="/login">로그인</Link>
+      <Link href="/login">로그인</Link> <br />
+      <Link href="/home">홈먼저 만들거야</Link>
     </div>
   );
 }
