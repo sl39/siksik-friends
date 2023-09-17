c902adb741d96798f3433a150886b9f041733629
구희영 <9hiyoung@gmail.com>
2023-09-17 20:47:25 +0900

    Feat: next-auth 구현중
    소셜 로그인 하는중 어렵다

--- front/src/containers/Login/LoginModal.tsx
+++ front/src/containers/Login/LoginModal.tsx
@@ -2,11 +2,24 @@
 
 import Link from "next/link";
 import { useState } from "react";
+
 import styles from "./Login.module.css";
 import Modal from "@/components/modal";
 import LoginForm from "./LoginForm";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Portal() {
+  // 세션을 이용하여 데이터를 불러온다.
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
   const [isOpen, setIsOpen] = useState(false);
 
   /** 모달 활성화 */
@@ -33,7 +46,8 @@ export default function Portal() {
             <Link href="/sign-up">
               <div>회원가입</div>
             </Link>
-            <div>카카오 로그인</div>
+            <button>카카오 로그인</button>
+
             {/* 닫기 버튼 로고로 바꿔야함 */}
             <button onClick={handleCloseModal} className={styles.closeButton}>
               X
