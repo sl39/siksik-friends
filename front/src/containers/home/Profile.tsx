c2dcca4faea70106481da08b6e7dd8fd85aa1e42
구희영 <9hiyoung@gmail.com>
2023-09-18 00:36:13 +0900

    Feat: Home 페이지 프로필 div 설정
    크기, 안의 div box 생성만

--- front/src/containers/home/Profile.tsx
+++ front/src/containers/home/Profile.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
 import styles from "./home.module.css";
 export default function Profile() {
+  // 데이터 페치 시 유저 정보를 불러와야 한다!!
   return (
     <div className={styles.profileContainer}>
-      <div>Profile</div>
+      <div className={styles.image}>
+        <Image src="" alt="캐릭터 프로필" />
+      </div>
+      <div className={styles.nickname}>
+        <span className={styles.level}>레벨</span>닉네임
+      </div>
     </div>
   );
 }
