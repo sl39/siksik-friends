b5e908b2e86553b1c7902749ae9c15413f2b7309
구희영 <9gmldud@naver.com>
2023-09-18 11:27:59 +0900

    Feat: axios 구현
    key 값이 한글이면 안되서 영어로 변경 후 테스트 완료

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,18 +1,24 @@
+"use client";
+
 import Login from "@/containers/Login/LoginModal";
 import { fetchData } from "@/services/api";
 import styles from "./page.module.css";
 
-export default function Home() {
-  const getData = async () => {
-    try {
-      const data = await fetchData("/920");
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  getData();
+interface UserData {
+  id: number;
+}
 
+const getUserData = async () => {
+  try {
+    const data = await fetchData<UserData>("/920/1", "GET");
+    console.log(data.id);
+  } catch (err) {
+    console.log(err);
+  }
+};
+getUserData();
+
+export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.Logo}>식식프렌즈 로고 가져오기</div>
--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,11 +1,17 @@
 // api.ts 파일
 import axios from "axios";
+import type { AxiosResponse, Method } from "axios";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_AXIOS_URL;
 
-export const fetchData = async (url: string) => {
+export const fetchData = async <T>(url: string, method: Method = "GET", data?: any, headers?: any): Promise<T> => {
   try {
-    const response = await axios.get(url);
+    const response: AxiosResponse<T> = await axios.request({
+      url,
+      method,
+      data,
+      headers,
+    });
     return response.data;
   } catch (error) {
     console.error(error);
