c392a0c675c6ab148e4fae4c5bc44b9b12951b51
kimta2hwan <kimta2hwan@gmail.com>
2023-09-18 10:43:58 +0900

    Refactor: MemberRepositoryTest 하드코딩 상수화
    MemberRepositoryTest 하드코딩 상수화

--- auth/src/test/java/com/ssf/auth/domain/member/repositoryTest/MemberRepositoryTest.java
+++ auth/src/test/java/com/ssf/auth/domain/member/repositoryTest/MemberRepositoryTest.java
@@ -15,6 +15,11 @@ import static org.assertj.core.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MemberRepositoryTest {
 
+    private static final String EMAIL = "test@test.com";
+    private static final String PASSWORD = "password";
+    private static final String NICKNAME = "nickname";
+    private static final String PROFILE = "/profile.png";
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -27,9 +32,9 @@ public class MemberRepositoryTest {
     public void 멤버등록() {
         // given
         final Member member = Member.builder()
-                .email("test@test.com")
-                .password("password")
-                .nickname("nickname")
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
                 .build();
 
         // when
@@ -37,10 +42,10 @@ public class MemberRepositoryTest {
 
         // then
         assertThat(result.getId()).isNotNull();
-        assertThat(result.getEmail()).isEqualTo("test@test.com");
-        assertThat(result.getPassword()).isEqualTo("password");
-        assertThat(result.getNickname()).isEqualTo("nickname");
-        assertThat(result.getProfile()).isEqualTo("/profile.png");
+        assertThat(result.getEmail()).isEqualTo(EMAIL);
+        assertThat(result.getPassword()).isEqualTo(PASSWORD);
+        assertThat(result.getNickname()).isEqualTo(NICKNAME);
+        assertThat(result.getProfile()).isEqualTo(PROFILE);
         assertThat(result.getSignUpAt()).isNotNull();
         assertThat(result.getUpdateAt()).isNotNull();
         assertThat(result.getRole()).isEqualTo(Role.GUEST);
@@ -51,21 +56,21 @@ public class MemberRepositoryTest {
     public void 멤버조회() {
         // given
         final Member member = Member.builder()
-                .email("test@test.com")
-                .password("password")
-                .nickname("nickname")
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
                 .build();
 
         // when
         memberRepository.save(member);
-        final Member findResult = memberRepository.findByEmailAndSocialType("test@test.com", SocialType.NONE);
+        final Member findResult = memberRepository.findByEmailAndSocialType(EMAIL, SocialType.NONE);
 
         // then
         assertThat(findResult.getId()).isNotNull();
-        assertThat(findResult.getEmail()).isEqualTo("test@test.com");
-        assertThat(findResult.getPassword()).isEqualTo("password");
-        assertThat(findResult.getNickname()).isEqualTo("nickname");
-        assertThat(findResult.getProfile()).isEqualTo("/profile.png");
+        assertThat(findResult.getEmail()).isEqualTo(EMAIL);
+        assertThat(findResult.getPassword()).isEqualTo(PASSWORD);
+        assertThat(findResult.getNickname()).isEqualTo(NICKNAME);
+        assertThat(findResult.getProfile()).isEqualTo(PROFILE);
         assertThat(findResult.getSignUpAt()).isNotNull();
         assertThat(findResult.getUpdateAt()).isNotNull();
         assertThat(findResult.getRole()).isEqualTo(Role.GUEST);
