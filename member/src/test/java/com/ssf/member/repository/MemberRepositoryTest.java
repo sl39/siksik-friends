3fd4c87a090d3c9ef2be49c07a57b1ba183dd445
swcl <swcl309@naver.com>
2023-09-18 14:27:22 +0900

    Refactor: MemberRepository 생성

--- member/src/test/java/com/ssf/member/repository/MemberRepositoryTest.java
+++ member/src/test/java/com/ssf/member/repository/MemberRepositoryTest.java
@@ -1,18 +1,21 @@
-package com.ssf.member.Repository;
+package com.ssf.member.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.assertj.core.api.Assertions.*;
 
-
+@DataJpaTest
 public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+
     @Test
     public void MemberRepository가null이아님(){
-        assertThat(membershipRepository).isNotNull();
+        assertThat(memberRepository).isNotNull();
     }
 
 }
