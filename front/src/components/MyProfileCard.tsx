974184588f1cb517cbc9409272db20757b780a44
구희영 <9gmldud@naver.com>
2023-09-18 14:34:36 +0900

    Feat:  Jotai 설치

--- front/src/components/MyProfileCard.tsx
+++ front/src/components/MyProfileCard.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import Image from "next/image";
+// import { useUpdateAtom } from "jotai";
+// import userAtom from "@/store/userAtom";
 import styles from "./MyProfileCard.module.css";
 
 export default function Profile() {
+  // const [user] = useUpdateAtom(userAtom);
+
+  // useEffect(() => {
+  //   setUser(fetchUser());
+  // }, []);
+
+  // if (!user) return <div>Loading...</div>;
+
   return (
     <div className={styles.profileContainer}>
       <div className={styles.image}>
         <Image src="" alt="캐릭터 프로필" />
       </div>
       <div className={styles.nickname}>
-        <span className={styles.level}>레벨</span>닉네임
+        <span className={styles.level}>레벨</span>
+        닉네임
       </div>
     </div>
   );
