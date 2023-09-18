b5ff24d962ba68a7adf98ae4fadac78ee40fd229
swcl <swcl309@naver.com>
2023-09-18 09:06:47 +0900

    Docs: 20230918

--- TIL/세울/spring/src/main/java/com/OAuth/spring/global/config/SecurityConfig.java
+++ TIL/세울/spring/src/main/java/com/OAuth/spring/global/config/SecurityConfig.java
@@ -1,58 +1,45 @@
 package com.OAuth.spring.global.config;
 
-import com.OAuth.spring.member.service.OAuth2UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) // securedEnabled는 secured어노테이션 활성화 prePostEnabled preAuthorized라는 어노테이션 활성화
 public class SecurityConfig {
-    private final OAuth2UserService oAuth2UserService;
-
-    public SecurityConfig(OAuth2UserService oAuth2UserService) {
-        this.oAuth2UserService = oAuth2UserService;
-    }
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(c -> c.disable())
+                .cors(c -> c.disable())
+                .headers(c -> c.frameOptions(f -> f.disable()).disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").authenticated()
+                        .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(login -> login
+                        .loginPage("/loginForm")
+                        .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행
+                        .defaultSuccessUrl("/")
 
-
-        http.csrf(AbstractHttpConfigurer::disable); // csrf 보안 설정 사용 X
-        http.authorizeHttpRequests(config -> config.anyRequest().permitAll());
-        http.oauth2Login(oauth2Login -> oauth2Login // OAuth2를 통한 로그인 사용
-                .loginPage("/login")
-                .successHandler(successHandler())
-                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint // 사용자가 로그인에 성공하였을 경우,
-                .userService(oAuth2UserService))); // 해당 서비스 로직을 타도록 설정
-
+                );
         return http.build();
     }
+
     @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return ((request, response, authentication) -> {
-            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-
-            String id = defaultOAuth2User.getAttributes().get("id").toString();
-            String body = """
-                    {"id":"%s"}
-                    """.formatted(id);
-
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-            PrintWriter writer = response.getWriter();
-            writer.println(body);
-            writer.flush();
-        });
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
     }
+
+
+
 }
---
+++ TIL/세울/spring/src/main/java/com/OAuth/spring/member/controller/IndexController.java
@@ -0,0 +1,80 @@
+package com.OAuth.spring.member.controller;
+
+import com.OAuth.spring.member.Repository.UserRepository;
+import com.OAuth.spring.member.entity.User;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public IndexController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole(ROLE_MANAGER)")
+    @GetMapping("/data")
+    public @ResponseBody String data(){
+        return "데이터";
+    }
+}
