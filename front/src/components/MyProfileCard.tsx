128de8d8709738fc1094c3816da1c6d14fdf253d
구희영 <9gmldud@naver.com>
2023-09-18 12:18:38 +0900

    Feat:  Jotai 설치
    전역 상태 관리로 Jotai 쓸거임

--- front/src/components/MyProfileCard.tsx
+++ front/src/components/MyProfileCard.tsx
@@ -1,14 +1,40 @@
+"use client";
+
 import Image from "next/image";
+// import { fetchData } from "@/services/api";
 import styles from "./MyProfileCard.module.css";
 
+// interface UserData {
+//   id: number;
+//   level: number;
+//   name: string;
+// }
+
 export default function Profile() {
+  // const [user, setUser] = useRecoilState(userAtom);
+  // const getUserId = 1;
+  // const getUserData = async () => {
+  //   try {
+  //     const data = await fetchData<UserData>(`/920/${getUserId}`, "GET");
+  //     console.log(data.id);
+  //     setUser(data);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getUserData();
+  // }, []);
+
   return (
     <div className={styles.profileContainer}>
       <div className={styles.image}>
         <Image src="" alt="캐릭터 프로필" />
       </div>
       <div className={styles.nickname}>
-        <span className={styles.level}>레벨</span>닉네임
+        <span className={styles.level}>레벨</span>
+        {닉네임}
       </div>
     </div>
   );
---
+++ front/src/states/recoil.ts
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+// 유저 데이터
+export const userAtom = atom({
+  key: "userAtom",
+  default: null,
+});
