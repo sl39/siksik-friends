ed8bca1f8e54b6711620159cf02d2c66f4412eeb
kimta2hwan <kimta2hwan@gmail.com>
2023-09-18 17:12:27 +0900

    Test: MemberController 존재 성공 테스트
    MemberController 존재 성공 테스트 코드 작성

---
+++ auth/src/main/java/com/ssf/auth/domain/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ssf.auth.domain.member.controller;
+
+public class MemberController {
+}
--- auth/src/test/java/com/ssf/auth/domain/member/ControllerTest/MemberControllerTest.java
+++ auth/src/test/java/com/ssf/auth/domain/member/ControllerTest/MemberControllerTest.java
@@ -1,16 +1,20 @@
 package com.ssf.auth.domain.member.ControllerTest;
 
 import com.google.gson.Gson;
+import com.ssf.auth.domain.member.controller.MemberController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberControllerTest {
 
+    @InjectMocks
     private MemberController target;
 
     private MockMvc mockMvc;
@@ -18,7 +22,9 @@ public class MemberControllerTest {
 
     @Test
     public void mockMvc가Null이아님() throws Exception {
-        assertThat(target).isNotnull();
+        mockMvc = MockMvcBuilders.standaloneSetup(target)
+                        .build();
+        assertThat(target).isNotNull();
         assertThat(mockMvc).isNotNull();
     }
 }
