6778e39aa4a0cfb1b71f189d2a38fa091c2222e5
swcl <swcl309@naver.com>
2023-09-18 16:05:27 +0900

    Test: 멤버 값 추가 실패

--- member/src/main/java/com/ssf/member/entity/Member.java
+++ member/src/main/java/com/ssf/member/entity/Member.java
@@ -1,12 +1,18 @@
 package com.ssf.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Table
+@NoArgsConstructor
+@Getter
+@Builder
+@AllArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- member/src/test/java/com/ssf/member/repository/MemberRepositoryTest.java
+++ member/src/test/java/com/ssf/member/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.ssf.member.repository;
 
+import com.ssf.member.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,6 +17,39 @@ public class MemberRepositoryTest {
     @Test
     public void MemberRepository가null이아님(){
         assertThat(memberRepository).isNotNull();
+    }
+
+    @Test
+    public void 멤버십등록(){
+        // given
+        final Member member = Member.builder()
+                .email("test@test.com")
+                .password("123")
+                .nickname("se")
+                .score(1000)
+                .activated(false)
+                .profile("image")
+                .role("MEMBER")
+                .social_type("kakao")
+                .sign_up_at("2023-09-18")
+                .update_at("2023-09-18");
+
+        //when
+        final Member result = memberRepository.save(member);
+
+        //then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getEmail()).isEqualTo("test@test.com");
+        assertThat(result.getPassword()).isEqualTo("123");
+        assertThat(result.getNickname()).isEqualTo("se");
+        assertThat(result.getScore()).isEqualTo(1000);
+        assertThat(result.getActivated()).isEqualTo(false);
+        assertThat(result.getProfile()).isEqualTo("image");
+        assertThat(result.getRole()).isEqualTo("MEMBER");
+        assertThat(result.getSocial_type()).isEqualTo("kakao");
+        assertThat(result.getSign_up_at()).isEqualTo("2023-09-18");
+        assertThat(result.getUpdate_at()).isEqualTo("2023-09-18");
+
     }
 
 }
