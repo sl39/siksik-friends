5ca0eb09a7e2392235b37a41fd2828f331ec3b52
kimta2hwan <kimta2hwan@gmail.com>
2023-09-19 09:45:48 +0900

    Docs: 테스트용 설정 파일 수정
    테스트용 설정 파일 application.yaml 파일 수정

--- auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
+++ auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
@@ -9,16 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.ssf.auth.domain.user.header.UserConstants.USER_ID_HEADER;
-
 @RestController
 public class UserController {
-
-    @PostMapping("/api/v1/member")
-    public ResponseEntity<UserResponse> createMember (
-            @RequestHeader(USER_ID_HEADER) final String id,
-            @RequestBody final UserRequest memberRequest) {
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 }
--- auth/src/main/java/com/ssf/auth/domain/user/service/UserService.java
+++ auth/src/main/java/com/ssf/auth/domain/user/service/UserService.java
@@ -1,42 +1,8 @@
 package com.ssf.auth.domain.user.service;
 
-import com.ssf.auth.domain.user.dto.UserResponse;
-import com.ssf.auth.domain.user.entity.User;
-import com.ssf.auth.domain.user.enums.UserErrorResult;
-import com.ssf.auth.domain.user.enums.SocialType;
-import com.ssf.auth.domain.user.exception.UserException;
-import com.ssf.auth.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.ssf.auth.domain.user.dto.UserSignUpDto;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository memberRepository;
-
-    private static final String EMAIL = "test@test.com";
-    private static final String PASSWORD = "password";
-    private static final String NICKNAME = "nickname";
-
-    public UserResponse createMember(final String email, final SocialType socialType) {
-        final User result = memberRepository.findByEmailAndSocialType(email, socialType);
-
-        if (result != null) {
-            throw new UserException(UserErrorResult.DUPLICATED_MEMBER_REGISTER);
-        }
-
-        final User member = User.builder()
-                .email(EMAIL)
-                .password(PASSWORD)
-                .nickname(NICKNAME)
-                .build();
-
-        final User savedMember =  memberRepository.save(member);
-
-        return UserResponse.builder()
-                .email(savedMember.getEmail())
-                .socialType(savedMember.getSocialType())
-                .build();
-    }
+    public void signUp(UserSignUpDto userSignUpDto) throws Exception;
 }
