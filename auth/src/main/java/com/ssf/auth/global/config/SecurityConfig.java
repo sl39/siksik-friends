d03275ef2c53bcc16de64ece95e479a6d24d5ac2
kimta2hwan <kimta2hwan@gmail.com>
2023-09-19 10:31:50 +0900

    Fix: H2 연결 오류 수정
    H2 와 Security 같이 쓰면 생기는 오류 수정
    RequestMatchers MvcRequestMatcher 로 명시

--- auth/src/main/java/com/ssf/auth/global/config/SecurityConfig.java
+++ auth/src/main/java/com/ssf/auth/global/config/SecurityConfig.java
@@ -3,14 +3,39 @@ package com.ssf.auth.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .securityMatcher("/**", "/api/**", "/login/**")
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/test/**").permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/**")).permitAll()
+                        .anyRequest().authenticated());
+//                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
