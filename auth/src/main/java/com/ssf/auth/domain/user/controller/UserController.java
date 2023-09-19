d2178725d849db1abb57bd79b366005594667932
kimta2hwan <kimta2hwan@gmail.com>
2023-09-19 10:32:21 +0900

    Feat: 회원가입 컨트롤러 구현
    회원가입 컨트롤러 코드 구현

--- auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
+++ auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.ssf.auth.domain.user.controller;
 
-import com.ssf.auth.domain.user.dto.UserRequest;
-import com.ssf.auth.domain.user.dto.UserResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.ssf.auth.domain.user.dto.UserSignUpDto;
+import com.ssf.auth.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
+        userService.signUp(userSignUpDto);
+        return "회원가입 성공";
+    }
 }
