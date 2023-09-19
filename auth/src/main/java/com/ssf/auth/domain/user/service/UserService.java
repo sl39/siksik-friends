fc1c8e2ec0b98a4f02fdc334515414b2376730fb
kimta2hwan <kimta2hwan@gmail.com>
2023-09-19 10:44:51 +0900

    Fix: 로그인 서비스 구현
    로그인 서비스 코드 작성

--- auth/src/main/java/com/ssf/auth/domain/user/service/UserService.java
+++ auth/src/main/java/com/ssf/auth/domain/user/service/UserService.java
@@ -1,8 +1,10 @@
 package com.ssf.auth.domain.user.service;
 
+import com.ssf.auth.domain.user.dto.UserSignInDto;
 import com.ssf.auth.domain.user.dto.UserSignUpDto;
 
 public interface UserService {
 
     public void signUp(UserSignUpDto userSignUpDto) throws Exception;
+    public void signIn(UserSignInDto userSignInDto) throws Exception;
 }
