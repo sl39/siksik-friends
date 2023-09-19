b0af2a32b92b1acdd68a6271c8d307f765781ae4
kimta2hwan <kimta2hwan@gmail.com>
2023-09-19 09:29:21 +0900

    Refactor: 회원 검색 간소화
    회원 검색 기능 간소화

--- auth/src/main/java/com/ssf/auth/domain/user/repository/UserRepository.java
+++ auth/src/main/java/com/ssf/auth/domain/user/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.ssf.auth.domain.user.enums.SocialType;
 import com.ssf.auth.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmailAndSocialType(final String email, final SocialType socialType);
+    Optional<User> findByEmail(String email);
 }
