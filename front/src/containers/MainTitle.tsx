74ac2340f0f27316c78b27e6ceca58234d88c7b0
구희영 <9gmldud@naver.com>
2023-09-21 08:57:59 +0900

    Fix: Lint 에러 수정
    빌드 오류 수정

--- front/src/containers/MainTitle.tsx
+++ front/src/containers/MainTitle.tsx
@@ -8,7 +8,8 @@ export default function MainTitle({ data }: Props) {
   return (
     <span className={styles.text}>
       {data.split("").map((char, i) => (
-        <div className={styles.letter} style={{ "--delay": `${i * 0.2}s` }}>
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i} className={styles.letter} style={{ "--delay": `${i * 0.2}s` } as React.CSSProperties}>
           <span className={[styles.source].join(" ")}>{char}</span>
           <span className={[styles.shadow].join(" ")}>{char}</span>
           <span className={[styles.overlay].join(" ")}>{char}</span>
