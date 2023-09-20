2794d893a54a3cac6b0c79974bdec5ac3cd83ee2
구희영 <9gmldud@naver.com>
2023-09-20 16:42:54 +0900

    Feat: 게임 대기실 모달 구현
    방찾기, 방 만들기 모달 구현

--- front/src/containers/Game/EnterRoom.tsx
+++ front/src/containers/Game/EnterRoom.tsx
@@ -1,35 +1,106 @@
 "use client";
 
 import { useState } from "react";
-
+import { useRouter } from "next/navigation";
 import styles from "./game.module.scss";
 import Modal from "@/components/gameModal";
+import { serverAxios } from "@/services/api";
 
 export default function EnterRoom() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [openCreateRoom, setOpenCreateRoom] = useState(false);
+  const [openSearchRoom, setOpenSearchRoom] = useState(false);
+  const [searchRoom, setSearchRoom] = useState("");
+  const router = useRouter();
 
-  /** 모달 활성화 */
-  const handleOpenModal = () => {
-    setIsOpen(true);
+  /** 게임 방 ID로 방 정보를 조회하는 함수 */
+  const handleSearchRoom = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(searchRoom);
+
+    try {
+      const response = await serverAxios.get("/");
+      console.log(response);
+    } catch (err) {
+      console.log("방 찾기 에러", err);
+    }
   };
-  /** 모달 비활성화 */
-  const handleCloseModal = () => {
-    setIsOpen(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    count: 0,
+  });
+  /** 게임 생성 */
+  const handleCreateGame = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    /** 게임방 POST 요청 */
+    try {
+      const response = await serverAxios.post("/", formData);
+      console.log(response);
+      const id = 1;
+      // 모달 종료 후 게임 방 입장
+      setOpenCreateRoom(false);
+      router.push(`/game/room/${id}`);
+    } catch (err) {
+      console.log(err);
+      // Axios 연결 전 임시 데이터
+      const id = 1;
+      // 모달 종료 후 게임 방 입장
+      setOpenCreateRoom(false);
+      router.push(`/game/room/${id}`);
+    }
   };
+
+  const handleChange = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    setFormData({ ...formData, [key]: value });
+  };
+
   return (
     <>
       <div>
         <button className={styles.boxButton}>빠른시작</button>
-        <button className={styles.boxButton}>방 찾기</button>
+        <button className={styles.boxButton} onClick={() => setOpenSearchRoom(true)}>
+          방 찾기
+        </button>
+        {openSearchRoom && (
+          <Modal isOpen={openSearchRoom}>
+            <div className={styles.modalContainer}>
+              <h1>방찾기</h1>
+              <form onSubmit={handleSearchRoom}>
+                <label htmlFor="room">방코드</label>
+                <input
+                  type="text"
+                  name="room"
+                  id="room"
+                  value={searchRoom}
+                  onChange={(e) => setSearchRoom(e.target.value)}
+                />
+                <button type="submit">찾기</button>
+              </form>
+              <button onClick={() => setOpenSearchRoom(false)}>취소</button>
+            </div>
+          </Modal>
+        )}
       </div>
       <div id="game-modal" className="z-99"></div>
-      <button onClick={handleOpenModal} className={styles.boxButton}>
+      <button onClick={() => setOpenCreateRoom(true)} className={styles.boxButton}>
         방 만들기
       </button>
-      {isOpen && (
-        <Modal isOpen={isOpen}>
-          <h2>(children임) 여기에 넣고싶은 내용 넣기</h2>
-          <button onClick={handleCloseModal}>Close modal</button>
+      {openCreateRoom && (
+        <Modal isOpen={openCreateRoom}>
+          <div className={styles.modalContainer}>
+            <h1>방만들기</h1>
+            <form onSubmit={handleCreateGame}>
+              <label htmlFor="title">제목</label>
+              <input type="text" name="title" id="title" value={formData.title} onChange={(e) => handleChange(e)} />
+              <label htmlFor="count">인원</label>
+              <input type="number" name="count" id="count" value={formData.count} onChange={(e) => handleChange(e)} />
+              <button type="submit">확인</button>
+            </form>
+            <button onClick={() => setOpenCreateRoom(false)}>취소</button>
+          </div>
         </Modal>
       )}
     </>
