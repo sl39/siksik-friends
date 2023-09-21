29b0d46e153944f64933d6f9f8b770b4df4dca4a
구희영 <9gmldud@naver.com>
2023-09-21 12:04:58 +0900

    Feat: 로그인 페이지 완
    CSS 폼 뺴고 다했다

--- front/src/app/sign-up/page.tsx
+++ front/src/app/sign-up/page.tsx
@@ -1,11 +1,50 @@
-import SignUp from "@/containers/SignUp";
+"use client";
 import styles from "@/app/page.module.scss";
+import BackButton from "@/components/BackButton";
+import LoginForm from "@/containers/SignUp/LoginForm";
+import SignUpForm from "@/containers/SignUp/SignUpForm";
+import { useState } from "react";
 
-// 회원가입은 모달을 닮은 페이지
 export default function Login() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
   return (
     <div className={styles.main}>
-      <SignUp />
+      <div className={styles.container}>
+        <div className={styles.label}>
+          <span>Log In</span>
+
+          <input
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className={styles.checkbox}
+            type="checkbox"
+            id="reg-log"
+            name="reg-log"
+          />
+          <label htmlFor="reg-log"></label>
+          <span>Sign Up</span>
+        </div>
+
+        <div className={styles.cardWrap}>
+          <div className={`${styles.cardWrapper} ${isChecked ? styles.rotate : ""}`}>
+            <div className={styles["card-front"]}>
+              <div className={styles["center-wrap"]}>
+                <LoginForm />
+              </div>
+            </div>
+            <div className={styles["card-back"]}>
+              <div className={styles["center-wrap"]}>
+                <SignUpForm />
+              </div>
+            </div>
+          </div>
+        </div>
+        <BackButton className={[styles.button, styles.btnAct].join(" ")} />
+      </div>
     </div>
   );
 }
--- front/src/store/authAtom.ts
+++ front/src/store/authAtom.ts
@@ -1,38 +1,38 @@
-// import { atom, useAtom } from "jotai";
-// import type { User } from "@/types/types";
+import { atom, useAtom } from "jotai";
+import type { User } from "@/types";
 
-// // auth 상태를 관리하는 atom
-// const authAtom = atom<User | null>(null);
+// auth 상태를 관리하는 atom
+const authAtom = atom<User | null>(null);
 
-// // auth 상태를 갱신하는 atom (초기, 로그인, 로그아웃 시)
-// const initAuthAtom = atom(null, async (get, set) => {
-//   const token = {
-//     access: sessionStorage.getItem("accessToken"),
-//     refresh: localStorage.getItem("refreshToken"),
-//   };
+// auth 상태를 갱신하는 atom (초기, 로그인, 로그아웃 시)
+const initAuthAtom = atom(null, async (get, set) => {
+  const token = {
+    access: sessionStorage.getItem("accessToken"),
+    refresh: localStorage.getItem("refreshToken"),
+  };
 
-//   if (token.access) {
-//     const authResponse = await getMe();
-//     if (authResponse.data) return set(authAtom, authResponse.data);
-//   }
+  if (token.access) {
+    const authResponse = await getMe();
+    if (authResponse.data) return set(authAtom, authResponse.data);
+  }
 
-//   if (token.refresh) {
-//     const refreshResponse = await refreshAccessToken();
-//     if (refreshResponse.accessToken) {
-//       sessionStorage.setItem("accessToken", refreshResponse.accessToken);
+  if (token.refresh) {
+    const refreshResponse = await refreshAccessToken();
+    if (refreshResponse.accessToken) {
+      sessionStorage.setItem("accessToken", refreshResponse.accessToken);
 
-//       const authResponse = await getMe();
-//       if (authResponse.data) return set(authAtom, authResponse.data);
-//     }
-//   }
+      const authResponse = await getMe();
+      if (authResponse.data) return set(authAtom, authResponse.data);
+    }
+  }
 
-//   return set(authAtom, null);
-// });
+  return set(authAtom, null);
+});
 
-// // 사용하는 곳에서 쉽게 확인할 수 있게 hook으로 모았다.
-// export const useAuthAtom = () => {
-//   const [auth] = useAtom(authAtom);
-//   const [, initAuth] = useAtom(initAuthAtom);
+// 사용하는 곳에서 쉽게 확인할 수 있게 hook으로 모았다.
+export const useAuthAtom = () => {
+  const [auth] = useAtom(authAtom);
+  const [, initAuth] = useAtom(initAuthAtom);
 
-//   return { auth, initAuth };
-// };
+  return { auth, initAuth };
+};
