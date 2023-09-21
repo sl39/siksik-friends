c39ad940c16bb99e835370d725f341e2c17d2ab1
구희영 <9gmldud@naver.com>
2023-09-21 14:23:38 +0900

    Feat: axios 인터셉터 구현

--- front/src/services/userApi.ts
+++ front/src/services/userApi.ts
@@ -1,18 +1,18 @@
-// import axios from "axios";
-// import type { AxiosError } from "axios";
+// // import axios from "axios";
+// // import type { AxiosError } from "axios";
 
 // // 유저 관련 API
 // // const API_PATH = process.env.REACT_APP_API_PATH!;
-// const API_PATH = "https://jsonplaceholder.typicode.com/";
+// // const API_PATH = "https://jsonplaceholder.typicode.com/";
 
 // export const userApi = axios.create({
 //   baseURL: `${API_PATH}/user`,
 //   timeout: 2000,
 // });
 
-// accessToken이 존재할 때, 해당 토큰을 헤더에 담아 요청할 수 있도록 해야 함
-// interceptor: 요청 이전의 처리와 응답 이후의 처리
-/** 요청 이전 헤더 세팅 */
+// // accessToken이 존재할 때, 해당 토큰을 헤더에 담아 요청할 수 있도록 해야 함
+// // interceptor: 요청 이전의 처리와 응답 이후의 처리
+// /** 요청 이전 헤더 세팅 */
 // userApi.interceptors.request.use((config) => {
 //   const accessToken = sessionStorage.getItem("accessToken");
 //   // eslint-disable-next-line no-param-reassign
@@ -41,22 +41,22 @@
 //   }
 // );
 
-// /** refreshToken 유효성에 따라 만료된 토큰 없애는 역할 동시 진행 */
-// export const refreshAccessToken = async () => {
-//   const refreshToken = localStorage.getItem("refreshToken");
-//   if (!refreshToken) {
-//     return Promise.reject();
-//   }
-//   try {
-//     const { data } = await userApi.get<RefreshResponse>("/refresh", {
-//       headers: { RefreshToken: `Bearer ${refreshToken}` },
-//     });
-//     sessionStorage.setItem("accessToken", data.accessToken);
-//     localStorage.setItem("refreshToken", data.refreshToken);
-//     return data;
-//   } catch {
-//     sessionStorage.removeItem("accessToken");
-//     localStorage.removeItem("refreshToken");
-//     return Promise.reject();
-//   }
-// };
+// // /** refreshToken 유효성에 따라 만료된 토큰 없애는 역할 동시 진행 */
+// // export const refreshAccessToken = async () => {
+// //   const refreshToken = localStorage.getItem("refreshToken");
+// //   if (!refreshToken) {
+// //     return Promise.reject();
+// //   }
+// //   try {
+// //     const { data } = await userApi.get<RefreshResponse>("/refresh", {
+// //       headers: { RefreshToken: `Bearer ${refreshToken}` },
+// //     });
+// //     sessionStorage.setItem("accessToken", data.accessToken);
+// //     localStorage.setItem("refreshToken", data.refreshToken);
+// //     return data;
+// //   } catch {
+// //     sessionStorage.removeItem("accessToken");
+// //     localStorage.removeItem("refreshToken");
+// //     return Promise.reject();
+// //   }
+// // };
