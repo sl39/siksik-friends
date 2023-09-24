6e425ffa7296dda20448095f897147414e1c74d5
kimta2hwan <kimta2hwan@gmail.com>
2023-09-24 21:01:49 +0900

    Feat: 랭킹 정보 구현 완료 랭킹 정보 레디스에 담기게 구현 완료

--- auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
+++ auth/src/main/java/com/ssf/auth/domain/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.ssf.auth.domain.user.controller;
 
-import com.ssf.auth.domain.user.dto.UserSignUpDto;
+import com.ssf.auth.domain.user.dto.UserDto;
 import com.ssf.auth.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +14,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
-        userService.signUp(userSignUpDto);
+    public String signUp(@RequestBody UserDto.Request userRequest) throws Exception {
+        userService.signUp(userRequest);
         return "회원가입 성공";
     }
 
