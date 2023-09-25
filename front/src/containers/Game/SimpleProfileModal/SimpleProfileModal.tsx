69f34ae2f50e6bf7fb44906d95d4bcbd2810184f
구희영 <9gmldud@naver.com>
2023-09-25 15:11:28 +0900

    Fix: format 에러 수정
    빌드 안되서 에러 수정

--- front/src/containers/Game/SimpleProfileModal/SimpleProfileModal.tsx
+++ front/src/containers/Game/SimpleProfileModal/SimpleProfileModal.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import { useAtom } from "jotai";
+// import { useAtom } from "jotai";
 import { useEffect } from "react";
 import { serverAxios } from "@/services/api";
-import { userAtom } from "@/store/userAtom";
+// import { userAtom } from "@/store/userAtom";
 import styles from "../game.module.scss";
 
 export default function SimpleProfileModal() {
-  const [user, setUser] = useAtom(userAtom);
+  // const [user, setUser] = useAtom(userAtom);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await serverAxios("/user/my-info");
         console.log(response);
-        setUser(response.data);
+        // setUser(response.data);
       } catch (err) {
         console.log("유저 정보 에러", err);
       }
@@ -26,15 +26,18 @@ export default function SimpleProfileModal() {
     // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const userdetail = (userObject: object) => {
-    return Object.entries(userObject).map(([key, value]) => (
-      <div key={key}>
-        {key} : {value}
-      </div>
-    ));
-  };
+  // const userdetail = (userObject: object) => {
+  //   return Object.entries(userObject).map(([key, value]) => (
+  //     <div key={key}>
+  //       {key} : {value}
+  //     </div>
+  //   ));
+  // };
 
-    return(<div className={styles.modalContainer}>
-        {userdetail(user)}
-      </div>)
+  return (
+    <div className={styles.modalContainer}>
+      testtest
+      {/* {userdetail(user)} */}
+    </div>
+  );
 }
--- front/src/containers/Profile/ProfileData.tsx
+++ front/src/containers/Profile/ProfileData.tsx
@@ -2,16 +2,16 @@
 
 import { useEffect } from "react";
 import { useParams } from "next/navigation";
-import { useAtom } from "jotai";
+// import { useAtom } from "jotai";
 import { serverAxios } from "@/services/api";
-import { userAtom } from "@/store/userAtom";
+// import { userAtom } from "@/store/userAtom";
 import styles from "./Profile.module.css";
 
 export default function ProfileData() {
   const params = useParams();
   const userNickname = params[0];
 
-  const [userData, setUserData] = useAtom(userAtom);
+  // const [userData, setUserData] = useAtom(userAtom);
 
   // 내 프로필 데이터를 가져오는 함수
   const FetchData = async () => {
