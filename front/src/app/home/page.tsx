d0fcbf6b0af6fc1f521fe6c1421b2892bc401a98
구희영 <9gmldud@naver.com>
2023-09-25 12:43:46 +0900

    Feat: SSR axios 테스트
    테스트 코드 작성

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,5 +1,20 @@
+// import axios from "axios";
 import HomeIndex from "@/containers/home";
+import { serverAxios } from "@/services/api";
 
-export default function Home() {
+export default async function Home() {
+  // const res = await axios.get("https://jsonplaceholder.typicode.com/users/1", {
+  //   headers: { "Cache-Control": "no-store" },
+  // });
+  try {
+    const res = await serverAxios.get("/user/my-info");
+    const { data } = res;
+    console.log("------------------------------------------------");
+    console.log(data);
+  } catch (err) {
+    console.log("------------------------------------------------");
+    console.error("내 정보 에러");
+  }
+
   return <HomeIndex />;
 }
