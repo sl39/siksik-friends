14435a2e242bf8166ffcb6311e70532f838bcaa3
구희영 <9gmldud@naver.com>
2023-09-26 04:55:53 +0900

    Feat: 프로필 카드 수정
    프로필 정보에 맞게 수정함

---
+++ front/src/components/ProfileCard.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { serverAxios } from "@/services/api";
+import type { User } from "@/types";
+import styles from "./MyProfileCard.module.css";
+
+export default function Profile() {
+  const params = useParams();
+  let userId = 0;
+  if (typeof params.id === "string") {
+    userId = parseInt(params.id, 10);
+  }
+  const [user, setUser] = useState<User>({ nickname: "Nickname" });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await serverAxios(`/user/${userId}`);
+        console.log(response);
+        setUser(response.data);
+      } catch (err) {
+        console.log("프로필 정보 에러", err);
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.polaroid}>
+        <div className={styles.img}>
+          <Image
+            className={styles.profileImg}
+            src={`${user?.profile || "/images/character/rabbit.png"}`}
+            alt="캐릭터 프로필"
+            fill
+            sizes="100%"
+            priority
+          />
+        </div>
+        <div className={styles.caption}>
+          {user?.level} {user?.nickname}
+        </div>
+      </div>
+    </div>
+  );
+}
