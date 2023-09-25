0cc1fbe8e230f31d304bd73de3343cf96dc6347e
kimta2hwan <kimta2hwan@gmail.com>
2023-09-25 12:24:31 +0900

    Fix: cors 에러 수정 cors 에러 수정

--- member/src/main/java/com/ssf/member/domain/user/controller/UserController.java
+++ member/src/main/java/com/ssf/member/domain/user/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     private final UserFindService userFindService;
