dc81be5bcac06868e0939e2cfda0e40e75aacf43
구희영 <9gmldud@naver.com>
2023-09-27 06:06:02 +0900

    Fix: 친구 조회 에 수정
    에러 수정

--- front/src/containers/Game/FriendsRequestModal.tsx
+++ front/src/containers/Game/FriendsRequestModal.tsx
@@ -16,6 +16,7 @@ export default function FriendsRequest({ onClose }: Props) {
       const response = await serverAxios("/user/friend/response");
       console.log(response);
       setMyResponse(response.data);
+      console.log(myResponse);
     } catch (err) {
       console.log("요청 목록 조회", err);
     }
@@ -25,6 +26,7 @@ export default function FriendsRequest({ onClose }: Props) {
       const response = await serverAxios("/user/friend/request");
       console.log(response);
       setMyRequest(response.data);
+      console.log(myRequest);
     } catch (err) {
       console.log("요청 받은 목록 조회", err);
     }
@@ -33,14 +35,13 @@ export default function FriendsRequest({ onClose }: Props) {
   useEffect(() => {
     fetchResponse();
     fetchRequest();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={styles.modalContainer}>
       <div>내가 한 요청 목록</div>
-      {myResponse}
       <div>내가 받은 요청 목록</div>
-      {myRequest}
       <button onClick={onClose}>닫기</button>
     </div>
   );
