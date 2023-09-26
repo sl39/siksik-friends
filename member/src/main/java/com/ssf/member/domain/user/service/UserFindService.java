2f9e2787d9866c0c1c41b85d8bbb93c73d4e3de0
kimta2hwan <kimta2hwan@gmail.com>
2023-09-26 09:56:48 +0900

    Feat: 닉네임으로 조회 기능 구현 닉네임으로 회원 조회 기능 구현

--- member/src/main/java/com/ssf/member/domain/user/service/UserFindService.java
+++ member/src/main/java/com/ssf/member/domain/user/service/UserFindService.java
@@ -9,6 +9,7 @@ public interface UserFindService {
 
     public UserDto.Response findMyInfo(String accessHeader);
     public UserDto.Response findUser(Long id);
+    public UserDto.Response findNickname(String nickname);
     public Long findRank(Long id);
     public List<UserDto.Response> findRankList();
 }
