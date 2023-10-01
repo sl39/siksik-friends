efc5ff0020f4ac2c9cbc5970ba9227b93afbd37e
구희영 <9gmldud@naver.com>
2023-10-01 16:48:00 +0900

    Feat: Music
    노래 넣음

--- front/src/components/Music.tsx
+++ front/src/components/Music.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { MdMusicNote, MdMusicOff } from "react-icons/md";
 import styled from "styled-components";
+import { atom, useAtom } from "jotai";
 
 const MusicContainer = styled.div`
   z-index: 90;
@@ -14,12 +15,42 @@ const MusicContainer = styled.div`
 `;
 const MusicButton = styled.button`
   position: relative;
-  s
 `;
+
+const isMusicPlayingAtom = atom(false);
+
 export default function Music() {
   // eslint-disable-next-line no-null/no-null
   const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [isMusicPlaying, setIsMusicPlaying] = useState(localStorage.getItem("isMusicPlaying") === "true");
+  const [isMusicPlaying, setIsMusicPlaying] = useAtom(isMusicPlayingAtom);
+
+  const playAudioOnce = () => {
+    // 노래 켬
+    if (!audioRef.current) {
+      audioRef.current = new Audio();
+      audioRef.current.src = "/music/BGM.wav";
+      audioRef.current.loop = true;
+      audioRef.current.volume = isMusicPlaying ? 0 : 1;
+    }
+
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsMusicPlaying(true);
+
+      // 클라이언트 사이드에서만 localStorage에 접근
+      if (typeof window !== "undefined") {
+        localStorage.setItem("isMusicPlaying", String(true));
+        document.body.removeEventListener("click", playAudioOnce);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      document.body.addEventListener("click", playAudioOnce);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleClick = () => {
     if (isMusicPlaying) {
@@ -38,7 +69,11 @@ export default function Music() {
       audioRef.current.play();
     }
     setIsMusicPlaying(!isMusicPlaying);
-    localStorage.setItem("isMusicPlaying", String(!isMusicPlaying));
+
+    // 클라이언트 사이드에서만 localStorage에 접근
+    if (typeof window !== "undefined") {
+      localStorage.setItem("isMusicPlaying", String(!isMusicPlaying));
+    }
   };
 
   return (
