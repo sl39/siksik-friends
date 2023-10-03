abb82d31841db3b333a11a31aeae071996433f7d
구희영 <9gmldud@naver.com>
2023-10-03 12:01:04 +0900

    Feat: 프로필 모달 추가중
    모달에 같은 내용 추가

--- front/src/containers/Game/SimpleProfileModal.tsx
+++ front/src/containers/Game/SimpleProfileModal.tsx
@@ -1,23 +1,33 @@
 "use client";
 
-// import { useAtom } from "jotai";
-import { useAtom } from "jotai";
-// import { userAtom } from "@/store/userAtom";
-import { userAtom } from "@/store/userAtom";
+import { AiOutlineClose } from "react-icons/ai";
+import type { User } from "@/types";
 import styles from "./modal.module.scss";
+import ProfileCard from "@/components/ProfileCard";
 
 interface Props {
   onClose: () => void;
+  user: User;
 }
 
-export default function SimpleProfileModal({ onClose }: Props) {
-  const [user] = useAtom(userAtom);
+export default function SimpleProfileModal({ onClose, user }: Props) {
+  // const [user] = useAtom(userAtom);
+
+  // 회원의 모든 정보 받아오기
 
   return (
-    <div className={styles.modalContainer}>
-      나다
-      {user.nickname}
-      <button onClick={onClose}>닫기</button>
+    <div className={`${styles.modalContainer} ${styles.profiileModal}`}>
+      <div className={styles.Card}>
+        <ProfileCard userProp={user} />
+      </div>
+      <div className={styles.data}>
+        또정보
+        <div className={styles.btns}>친구요청</div>
+      </div>
+
+      <button className={styles.close} onClick={onClose}>
+        <AiOutlineClose size={48} />
+      </button>
     </div>
   );
 }
