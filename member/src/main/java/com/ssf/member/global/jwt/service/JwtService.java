a0a0fd33d719d55248613fa1ce1b106d37ceed41
kimta2hwan <kimta2hwan@gmail.com>
2023-10-04 01:32:50 +0900

    Refactor: 필요없는 기능 제거
    사용하지 않는 필요없는 기능 제거

--- member/src/main/java/com/ssf/member/global/jwt/service/JwtService.java
+++ member/src/main/java/com/ssf/member/global/jwt/service/JwtService.java
@@ -6,18 +6,14 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.ssf.member.domain.user.dto.UserRequest;
 import com.ssf.member.domain.user.repository.UserRepository;
 import com.ssf.member.global.jwt.dto.JwtDto;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 import static com.ssf.member.global.jwt.domain.JwtConstants.*;
 
@@ -43,32 +39,7 @@ public class JwtService {
     @Value("${jwt.refresh.header}")
     private String refreshHeader;
 
-    private final RedisTemplate<String, String> redisTemplate;
     private final UserRepository userRepository;
-
-    public String createAccessToken(String id) {
-        Date now = new Date();
-
-        return JWT.create()
-                .withSubject(ACCESS_TOKEN_SUBJECT.getValue())
-                .withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod))
-                .withClaim(ID_CLAIM.getValue(), Long.parseLong(id))
-                .sign(Algorithm.HMAC512(secretKey));
-    }
-
-    public String createRefreshToken() {
-        Date now = new Date();
-
-        return JWT.create()
-                .withSubject(REFRESH_TOKEN_SUBJECT.getValue())
-                .withExpiresAt(new Date(now.getTime() + refreshTokenExpirationPeriod))
-                .sign(Algorithm.HMAC512(secretKey));
-    }
-
-    public void sendAccessToken(HttpServletResponse response, String accessToken) {
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setHeader(accessHeader, accessToken);
-    }
 
     public JwtDto extractHeader(UserRequest.AccessHeader accessHeader) {
         String accessToken = extractAccessToken(accessHeader).orElseThrow();
@@ -105,9 +76,5 @@ public class JwtService {
     public Long extractExpiration(DecodedJWT decodedJWT) {
         return decodedJWT.getClaim(EXP_CLAIM.getValue()).asLong() * 1000
                 - System.currentTimeMillis();
-    }
-
-    public void updateRefreshToken(String id, String refreshToken) {
-        redisTemplate.opsForValue().set(id, refreshToken, 300L, TimeUnit.SECONDS);
     }
 }
