af2a674b44c2c6b2a58ee3f4b8edc259c0f8c358
권인식 <kwon2566@naver.com>
2023-10-04 16:20:26 +0900

    Fix: Testing Server

--- socket/src/main/java/com/ssf/socket/SocketApplication.java
+++ socket/src/main/java/com/ssf/socket/SocketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class SocketApplication {
 	public static void main(String[] args) {
