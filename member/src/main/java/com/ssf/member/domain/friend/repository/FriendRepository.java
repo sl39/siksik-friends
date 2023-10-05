2dc052fa3f5e0665124e9fc3028fd762c48ad2de
kimta2hwan <kimta2hwan@gmail.com>
2023-10-05 10:33:45 +0900

    Fix: 친구 목록 오류 수정
    친구 목록 오류 수정 완료

--- member/src/main/java/com/ssf/member/domain/friend/repository/FriendRepository.java
+++ member/src/main/java/com/ssf/member/domain/friend/repository/FriendRepository.java
@@ -10,9 +10,10 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     List<Friend> findAllByToUserIdAndActivated(Long toUserId, boolean activated);
     List<Friend> findAllByUser_IdAndActivated(Long fromUserId, boolean activated);
+    boolean existsByToUserIdAndUser_IdAndActivated(Long toUserId, Long fromUserId, boolean activated);
     Optional<Friend> findByToUserIdAndUser_Id(Long toUserId, Long fromUserId);
-    public void deleteByUser_IdAndToUserId(Long fromUserId, Long toUserId);
-    public void deleteByToUserId(Long userId);
-    public void deleteByUser_Id(Long userId);
-    public boolean existsByUser_IdAndToUserIdAndActivated(Long fromUserId, Long toUserId, boolean activated);
+    void deleteByUser_IdAndToUserId(Long fromUserId, Long toUserId);
+    void deleteByToUserId(Long userId);
+    void deleteByUser_Id(Long userId);
+    boolean existsByUser_IdAndToUserIdAndActivated(Long fromUserId, Long toUserId, boolean activated);
 }
