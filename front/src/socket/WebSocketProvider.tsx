273d9984a87ed09d958c4d8757c027edb7d0d732
권인식 <kwon2566@naver.com>
2023-10-09 13:38:57 +0900

    Fix: console log

--- front/src/socket/WebSocketProvider.tsx
+++ front/src/socket/WebSocketProvider.tsx
@@ -21,33 +21,37 @@ export default function WebSocketProvider({ children }: WebSocketProviderProps) 
   const [stompClient, setStompClient] = useState<WebSocketContextType | undefined>(undefined);
 
   useEffect(() => {
-    const socket = new SockJS("https://j9e101.p.ssafy.io/api/socket/ws");
-    const accessToken = sessionStorage.getItem("accessToken");
-    console.log(accessToken);
-    const headers = {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store",
-      authorization: `Bearer ${accessToken}`,
-    };
+    const connect = async () => {
+      try {
+        const socket = new SockJS("https://j9e101.p.ssafy.io/api/socket/ws");
+        const accessToken = sessionStorage.getItem("accessToken");
+        console.log(accessToken)
+        console.log(location.origin)
+        const headers = {
+          "Content-Type": "application/json",
+          "Cache-Control": "no-store",
+          Authorization: `Bearer ${accessToken}`,
+        };
 
-    const client = Stomp.over(socket);
-    // client.debug = () => {};
-    // client.configure({
-    //    reconnectDelay: 5000,
-    // });
-    function connect() {
-      client.connect(headers, function connection() {
+        const client = Stomp.over(socket);
+
+        await new Promise((resolve) => {
+          client.connect(headers, resolve);
+        });
         setStompClient(client);
-      });
-    }
+      } catch (err) {
+        console.log("소켓 에러", err);
+      }
+    };
 
     connect();
 
     return () => {
-      if (client) {
-        client.disconnect();
+      if (stompClient) {
+        stompClient.disconnect();
       }
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <WebSocketContext.Provider value={stompClient}>{children}</WebSocketContext.Provider>;
